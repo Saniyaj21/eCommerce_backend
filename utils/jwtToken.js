b430c25a4527c55c064fb185155a3dbcd69989d8
saniyaj mallik <saniyajmallik12@gmail.com>
2023-10-20 08:17:06 +0530

    cookie solved

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -3,17 +3,13 @@
 const sendToken = (user, statusCode, res) => {
   const token = user.getJWTToken();
 
-  // options for cookie
-  const options = {
-    expires: new Date(
+  res.cookie("token", token, {
+    maxAge: new Date(
       Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
-    // sameSite: "None",
-  };
-
-  res.cookie("token", token, {
-    options
+    sameSite: "None",
+    secure: true
   }).status(statusCode).json({
     success: true,
     user,
